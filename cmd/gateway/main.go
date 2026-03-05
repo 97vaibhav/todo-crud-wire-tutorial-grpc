b2d62b422260f5f704e43e503d93016df827f70d
Vaibhav Gupta <1997guptavaibhav@gmail.com>
2026-03-05 15:19:52 +0900

    feat: integrate OpenAPI documentation for Todo and Auth services
    - Added Swagger JSON specifications for Todo and Auth services to enable API documentation.
    - Implemented merging of Swagger specs in the gateway service to provide a unified API documentation endpoint.
    - Updated Makefile to include installation of the OpenAPI v2 plugin for gRPC gateway.
    - Enhanced the main gateway service to serve Swagger UI and JSON documentation.

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	_ "embed"
+	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
@@ -13,6 +15,93 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
+
+//go:embed api/todo/v1/todo.swagger.json
+var todoSwagger []byte
+
+//go:embed api/auth/v1/auth.swagger.json
+var authSwagger []byte
+
+func init() {
+	merged, err := mergeSwaggerSpecs(todoSwagger, authSwagger)
+	if err != nil {
+		log.Fatalf("merge swagger specs: %v", err)
+	}
+	mergedSwaggerJSON = merged
+}
+
+var mergedSwaggerJSON []byte
+
+// mergeSwaggerSpecs combines two OpenAPI 2.0 (Swagger) JSON specs into one.
+func mergeSwaggerSpecs(a, b []byte) ([]byte, error) {
+	var specA, specB map[string]interface{}
+	if err := json.Unmarshal(a, &specA); err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(b, &specB); err != nil {
+		return nil, err
+	}
+	pathsA, _ := specA["paths"].(map[string]interface{})
+	pathsB, _ := specB["paths"].(map[string]interface{})
+	if pathsA == nil {
+		pathsA = make(map[string]interface{})
+	}
+	for k, v := range pathsB {
+		pathsA[k] = v
+	}
+	specA["paths"] = pathsA
+	defsA, _ := specA["definitions"].(map[string]interface{})
+	defsB, _ := specB["definitions"].(map[string]interface{})
+	if defsA == nil {
+		defsA = make(map[string]interface{})
+	}
+	for k, v := range defsB {
+		defsA[k] = v
+	}
+	specA["definitions"] = defsA
+	tagsA, _ := specA["tags"].([]interface{})
+	tagsB, _ := specB["tags"].([]interface{})
+	if tagsA == nil {
+		tagsA = []interface{}{}
+	}
+	specA["tags"] = append(tagsA, tagsB...)
+	specA["info"] = map[string]interface{}{
+		"title":   "Todo + Auth API",
+		"version": "1.0",
+	}
+	// Add Bearer JWT security so Swagger UI shows the "Authorize" button.
+	// After login, click Authorize and enter: Bearer <your_access_token>
+	specA["securityDefinitions"] = map[string]interface{}{
+		"bearerAuth": map[string]interface{}{
+			"type":        "apiKey",
+			"name":        "Authorization",
+			"in":          "header",
+			"description": "Enter: Bearer <access_token> (from POST /api/v1/auth/login)",
+		},
+	}
+	specA["security"] = []interface{}{
+		map[string]interface{}{"bearerAuth": []interface{}{}},
+	}
+	return json.Marshal(specA)
+}
+
+const swaggerUIHTML = `<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+</head>
+<body>
+  <div id="swagger-ui"></div>
+  <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+  <script>
+    SwaggerUIBundle({
+      url: '/swagger.json',
+      dom_id: '#swagger-ui',
+    });
+  </script>
+</body>
+</html>
+`
 
 func main() {
 	cfg, err := config.Load()
@@ -44,9 +133,24 @@ func main() {
 		log.Fatalf("register auth service gateway: %v", err)
 	}
 
+	root := http.NewServeMux()
+	root.HandleFunc("/swagger.json", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(mergedSwaggerJSON)
+	})
+	root.HandleFunc("/docs", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(swaggerUIHTML))
+	})
+	root.HandleFunc("/docs/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/docs", http.StatusMovedPermanently)
+	})
+	root.Handle("/", mux)
+
 	addr := ":" + cfg.GatewayPort
 	log.Printf("REST API gateway listening on %s (backend gRPC: %s)", addr, cfg.GRPCBackendAddr)
-	if err := http.ListenAndServe(addr, mux); err != nil && err != http.ErrServerClosed {
+	log.Printf("Swagger UI: http://localhost%s/docs  spec: http://localhost%s/swagger.json", addr, addr)
+	if err := http.ListenAndServe(addr, root); err != nil && err != http.ErrServerClosed {
 		log.Fatalf("gateway serve: %v", err)
 	}
 }
